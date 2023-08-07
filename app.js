6c942394402e06053e1308a9b71d75ee24964a64
Andrey Shatskikh <ponchic-09@mail.ru>
2023-08-07 15:11:44 +0300

    fix: пятая попытка автотестов

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
 app.use('*', (req, res) => {
-  res.status(404).send({ message: `Страница не найдена` });
+  res.status(404).send({ message: 'Страница не найдена' });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
